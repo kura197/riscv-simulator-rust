eea5a20dce31d92cde48c6955d1930e4cd98cda6
kuramochi <kura19971106@gmail.com>
2021-07-28 16:27:57 +0900

    add some instructions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 
 
+#[derive(Debug)]
 pub struct Regfile {
     x: [u32; 32],
     pc: u32
@@ -71,16 +72,104 @@ pub fn decode(instr: u32) -> Option<Operand> {
         //TODO
         0b1100011 => Some(Operand::new("BRANCH", opcode, 0, 0, 0, 0, 0, 0)),
         0b0000011 => Some(Operand::new("LOAD", opcode, retrieve(instr, 7, 5), retrieve(instr, 12, 3), retrieve(instr, 15, 5), 0, 0, retrieve(instr, 20, 12))),
-        //TODO
-        0b0100011 => Some(Operand::new("STORE", opcode, 0, 0, 0, 0, 0, 0)),
+        0b0100011 => Some(Operand::new("STORE", opcode, 0, retrieve(instr, 12, 3), retrieve(instr, 15, 5), retrieve(instr, 20, 5), 0, (retrieve(instr, 25, 7) << 5 ) | retrieve(instr, 7, 5))),
         0b0010011 => Some(Operand::new("OP-IMM", opcode, retrieve(instr, 7, 5), retrieve(instr, 12, 3), retrieve(instr, 15, 5), 0, 0, retrieve(instr, 20, 12))),
+        0b0110011 => Some(Operand::new("OP", opcode, retrieve(instr, 7, 5), retrieve(instr, 12, 3), retrieve(instr, 15, 5), retrieve(instr, 20, 5), retrieve(instr, 25, 7), retrieve(instr, 20, 12))),
         //TODO
         0b0001111 => Some(Operand::new("MISC-MEM", opcode, 0, 0, 0, 0, 0, 0)),
         //TODO
         0b1110011 => Some(Operand::new("SYSTEM", opcode, 0, 0, 0, 0, 0, 0)),
         _ => {
-            println!("not supported.");
+            panic!("opcode {} is not supported.", opcode);
             None
         },
+    }
+}
+
+pub fn execute(regfile: &mut Regfile, dmem: &mut Vec<u8>, operand: &Operand) {
+    match operand.name.as_str() {
+        "LOAD" => execute_load(regfile, dmem, &operand),
+        "STORE" => execute_store(regfile, dmem, &operand),
+        "OP-IMM" => execute_op_imm(regfile, &operand),
+        "OP" => execute_op(regfile, &operand),
+        _ => panic!("operand name {} is not supported", operand.name),
+    }
+}
+
+fn execute_load(regfile: &mut Regfile, dmem: &mut Vec<u8>, operand: &Operand) {
+    let (rd, rs1) = (operand.rd as usize, operand.rs1 as usize);
+    let addr = (regfile.x[rs1] + operand.imm) as usize;
+    match operand.funct3 {
+        //// TODO: support sign-extend
+        //// TODO: check endian
+        0b000 => regfile.x[rd] = dmem[addr] as u32, // LB
+        0b001 => {  // LH
+            if addr % 2 != 0 {
+                panic!("addr {} need to be aligned by 2.", addr);
+            }
+            regfile.x[rd] = dmem[addr] as u32;
+            regfile.x[rd] |= (dmem[addr+1] as u32) << 8;
+        },
+        0b010 => {  // LW
+            if addr % 4 != 0 {
+                panic!("addr {} need to be aligned by 4.", addr);
+            }
+            regfile.x[rd] = dmem[addr] as u32;
+            regfile.x[rd] |= (dmem[addr+1] as u32) << 8;
+            regfile.x[rd] |= (dmem[addr+2] as u32) << 16;
+            regfile.x[rd] |= (dmem[addr+3] as u32) << 24;
+        },
+        0b100 => regfile.x[rd] = dmem[addr] as u32, // LBU
+        0b101 => {  // LHU
+            if addr % 2 != 0 {
+                panic!("addr {} need to be aligned by 2.", addr);
+            }
+            regfile.x[rd] = dmem[addr] as u32;
+            regfile.x[rd] |= (dmem[addr+1] as u32) << 8;
+        },
+        _ => panic!("funct3 {} is not supported.", operand.funct3),
+    }
+}
+
+fn execute_store(regfile: &mut Regfile, dmem: &mut Vec<u8>, operand: &Operand) {
+    let (rs1, rs2) = (operand.rs1 as usize, operand.rs2 as usize);
+    let addr = (regfile.x[rs1] + operand.imm) as usize;
+    match operand.funct3 {
+        //// TODO: support sign-extend for imm
+        //// TODO: check endian
+        0b000 => dmem[addr] = (regfile.x[rs2] & 0xFF) as u8,    // SB
+        0b001 => {  // SH
+            if addr % 2 != 0 {
+                panic!("addr {} need to be aligned by 2.", addr);
+            }
+            dmem[addr] = (regfile.x[rs2] & 0xFF) as u8;
+            dmem[addr+1] = ((regfile.x[rs2] >> 8) & 0xFF) as u8;
+        },
+        0b010 => {  // SW
+            if addr % 4 != 0 {
+                panic!("addr {} need to be aligned by 4.", addr);
+            }
+            dmem[addr] = (regfile.x[rs2] & 0xFF) as u8;
+            dmem[addr+1] = ((regfile.x[rs2] >> 8) & 0xFF) as u8;
+            dmem[addr+2] = ((regfile.x[rs2] >> 16) & 0xFF) as u8;
+            dmem[addr+3] = ((regfile.x[rs2] >> 24) & 0xFF) as u8;
+        },
+        _ => panic!("funct3 {} is not supported.", operand.funct3),
+    }
+}
+
+fn execute_op_imm(regfile: &mut Regfile, operand: &Operand) {
+    let (rd, rs1, rs2) = (operand.rd as usize, operand.rs1 as usize, operand.rs2 as usize);
+    match operand.funct3 {
+        0b000 => regfile.x[rd] = regfile.x[rs1] + operand.imm,  // ADDI
+        _ => panic!("funct3 {} is not supported.", operand.funct3),
+    }
+}
+
+fn execute_op(regfile: &mut Regfile, operand: &Operand) {
+    let (rd, rs1, rs2) = (operand.rd as usize, operand.rs1 as usize, operand.rs2 as usize);
+    match (operand.funct7, operand.funct3) {
+        (0b0000000, 0b000) => regfile.x[rd] = regfile.x[rs1] + regfile.x[rs2],  // ADD
+        _ => panic!("(funct7, funct3) ({}, {}) is not supported.", operand.funct7, operand.funct3),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::env;
 
 use riscv_simulator::*;
 
+const MEMSIZE: usize = 65536;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -14,15 +16,18 @@ fn main() {
 
     let filename = &args[1];
     let imem = fs::read(filename).expect("Unable to read file");
+    let mut dmem: Vec<u8> = vec![0; MEMSIZE];
 
     let mut regfile = Regfile::new(52);
 
     while regfile.get_pc()+3 < imem.len() as u32 {
+        println!("PC: {}", regfile.get_pc());
         let instr = regfile.get_next_instr(&imem);
-        println!("{:08x}", instr);
+        //println!("{:08x}", instr);
         let operand = decode(instr).unwrap();
         println!("{:?}", operand);
-        //// execute(&mut regfile, &operand);
+        execute(&mut regfile, &mut dmem, &operand);
+        println!("{:?}", regfile);
         regfile.inc_pc();
     }
 }
